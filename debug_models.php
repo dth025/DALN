4836c858855b39ff53f846a6cf0cd688cc124f45
dovanduy2005 <23017211@st.phenikaa-uni.edu.vn>
2026-06-03 21:01:25 +0700

    Dọn dẹp các tệp test không liên quan

--- debug_models.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$apiKey = 'AIzaSyAwG5CBYYfBNfGb48O3fMaIs8u7LA3NDj8';
-$url = "https://generativelanguage.googleapis.com/v1beta/models?key=$apiKey";
-$response = file_get_contents($url);
-echo $response;
